f8b1a239a055be2c31de6fa5e820e83d27b12c46
Anatoly Cherednichenko <shysatori@outlook.com>
2013-06-12 20:04:51 +1100

    It Works!
    можно увеличивать число с помощью коннекта.

--- TCP.Server/TCP.Server/Form1.Designer.cs
+++ TCP.Server/TCP.Server/Form1.Designer.cs
@@ -28,34 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.components = new System.ComponentModel.Container();
+            this.TurnOffButton = new System.Windows.Forms.Button();
+            this.TurnOnButton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.NowConnectedLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // TurnOffButton
             // 
-            this.button1.Location = new System.Drawing.Point(7, 10);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(275, 159);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Выключить серв";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.TurnOffButton.Location = new System.Drawing.Point(335, 340);
+            this.TurnOffButton.Name = "TurnOffButton";
+            this.TurnOffButton.Size = new System.Drawing.Size(112, 23);
+            this.TurnOffButton.TabIndex = 0;
+            this.TurnOffButton.Text = "Выключить серв";
+            this.TurnOffButton.UseVisualStyleBackColor = true;
+            this.TurnOffButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // TurnOnButton
+            // 
+            this.TurnOnButton.Location = new System.Drawing.Point(212, 340);
+            this.TurnOnButton.Name = "TurnOnButton";
+            this.TurnOnButton.Size = new System.Drawing.Size(117, 23);
+            this.TurnOnButton.TabIndex = 1;
+            this.TurnOnButton.Text = "Включить сервер";
+            this.TurnOnButton.UseVisualStyleBackColor = true;
+            this.TurnOnButton.Click += new System.EventHandler(this.TurnOnButton_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // NowConnectedLabel
+            // 
+            this.NowConnectedLabel.AutoSize = true;
+            this.NowConnectedLabel.Location = new System.Drawing.Point(209, 324);
+            this.NowConnectedLabel.Name = "NowConnectedLabel";
+            this.NowConnectedLabel.Size = new System.Drawing.Size(73, 13);
+            this.NowConnectedLabel.TabIndex = 2;
+            this.NowConnectedLabel.Text = "Подключено:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(459, 375);
+            this.Controls.Add(this.NowConnectedLabel);
+            this.Controls.Add(this.TurnOnButton);
+            this.Controls.Add(this.TurnOffButton);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button TurnOffButton;
+        private System.Windows.Forms.Button TurnOnButton;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label NowConnectedLabel;
     }
 }
 
--- TCP.Server/TCP.Server/Form1.cs
+++ TCP.Server/TCP.Server/Form1.cs
@@ -16,43 +16,49 @@ namespace TCP.Server
 {
     public partial class Form1 : Form
     {
-        public bool turnOff = false;
-        public int a = 0;
+        public List<TcpClient> AllClients = new List<TcpClient>();
+        public List<NetworkStream> AllStreams = new List<NetworkStream>();
+        public Task<TcpClient> taskWaiting;
+        private TcpListener server;
         public Form1()
         {
             InitializeComponent();
-            this.Show();
-            Thread ownThread = new Thread(new ThreadStart(thread1));
-            ownThread.Start();
         }
-        public void thread1()
+        public void ServerStart()
         {
             try
             {
-                TcpListener server = new TcpListener(IPAddress.Parse("127.0.0.1"), 4242);
+                server = new TcpListener(IPAddress.Parse("127.0.0.1"), 4242);
                 server.Start();
-                TcpClient client = server.AcceptTcpClient();
-                while (!turnOff)
-                {
-                    a++;
-                    NetworkStream stream = client.GetStream();
-                }
-                
+                taskWaiting = server.AcceptTcpClientAsync();
+                timer1.Start();
             }
             catch (SocketException e)
             {
                 Console.WriteLine("SocketException: {0}", e);
-            }
-            finally
-            {
-                //         MessageBox.Show("Сервер успешно запущен.");
             }          
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            turnOff = true;
-            this.Close();
+            
+        }
+
+        private void TurnOnButton_Click(object sender, EventArgs e)
+        {
+            ServerStart();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            AllClients = AllClients.Where(z => z.Connected).ToList();
+            NowConnectedLabel.Text = AllClients.Count.ToString();
+            if (taskWaiting.IsCompleted)
+            {
+                AllClients.Add(taskWaiting.Result);
+                NowConnectedLabel.Text = AllClients.Count.ToString();
+                taskWaiting = server.AcceptTcpClientAsync();
+            }
         }
     }
 }
